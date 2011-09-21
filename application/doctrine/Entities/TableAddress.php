7fa5ab5be0a67cbb2d48910c1b6e0cb5cb69e395
sarora <sarora@pcisa-160.isa-hamburg.lan>
2011-09-21 17:03:50 +0200

    neue entities

---
+++ application/doctrine/Entities/TableAddress.php
@@ -0,0 +1,79 @@
+<?php
+namespace Entities;
+
+/**
+ * TablePerson
+ *
+ * @Table(name="table_address")
+ * @Entity(repositoryClass="Entities\Repositories\TableAddressRepository")
+ */
+class TableAddress {
+    //put your code here
+     //put your code here
+    /**
+     * @var integer $id
+     *
+     * @Column(name="id", type="integer", nullable=false)
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var string 
+     *
+     * @Column(name="street", type="string", length=255, nullable=false)
+     */
+    protected $street;
+    
+    
+    /**
+     * @var string 
+     *
+     * @Column(name="city", type="string", length=255, nullable=false)
+     */
+    protected $city;
+
+    /**
+     *
+     * @var type 
+     * @ManyToMany(targetEntity="Entities\TablePerson", mappedBy="addresses")
+     */
+    protected $persons;
+    
+    public function getId() {
+	return $this->id;
+    }
+
+    public function setId($id) {
+	$this->id = $id;
+    }
+
+    public function getStreet() {
+	return $this->street;
+    }
+
+    public function setStreet($street) {
+	$this->street = $street;
+    }
+
+    public function getCity() {
+	return $this->city;
+    }
+
+    public function setCity($city) {
+	$this->city = $city;
+    }
+
+    public function getPersons() {
+	return $this->persons;
+    }
+
+    public function setPersons($persons) {
+	$this->persons = $persons;
+    }
+
+
+}
+
+?>
---
+++ application/doctrine/Entities/TablePerson.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Entities;
+
+/**
+ * TablePerson
+ *
+ * @Table(name="table_person")
+ * @Entity(repositoryClass="Entities\Repositories\TablePersonRepository")
+ */
+class TablePerson {
+
+    //put your code here
+    /**
+     * @var integer $id
+     *
+     * @Column(name="id", type="integer", nullable=false)
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var string $name
+     *
+     * @Column(name="name", type="string", length=255, nullable=false)
+     */
+    protected $name;
+
+    /**
+     * @var string $fname
+     *
+     * @Column(name="fname", type="string", length=255, nullable=false)
+     */
+    protected $fname;
+
+    /**
+     *
+     * @var type 
+     * @ManyToMany(targetEntity="Entities\TableAddress", inversedBy="persons")
+     * @JoinTable(name="table_person_address",
+     *      joinColumns={@JoinColumn(name="person_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@JoinColumn(name="address_id", referencedColumnName="id")}
+     *      )
+     */
+    protected $addresses;
+
+    public function getId() {
+	return $this->id;
+    }
+
+    public function setId($id) {
+	$this->id = $id;
+    }
+
+    public function getName() {
+	return $this->name;
+    }
+
+    public function setName($name) {
+	$this->name = $name;
+    }
+
+    public function getFname() {
+	return $this->fname;
+    }
+
+    public function setFname($fname) {
+	$this->fname = $fname;
+    }
+
+    public function getAddresses() {
+	return $this->addresses;
+    }
+
+    public function setAddresses($addresses) {
+	$this->addresses = $addresses;
+    }
+
+
+}
+
+?>
